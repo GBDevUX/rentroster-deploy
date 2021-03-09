54429d8cbe3e00b8131ad2530b49a35079286881
Gavin <gbdevux@gmail.com>
2021-03-08 19:14:22 -0500

    resploy and remove settings

--- src/graphql/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resolvers = void 0;
-const lodash_merge_1 = __importDefault(require("lodash.merge"));
-const Listing_1 = require("./Listing");
-exports.resolvers = lodash_merge_1.default(Listing_1.listingResolvers);
